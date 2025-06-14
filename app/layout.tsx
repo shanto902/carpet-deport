3a6a18a3747a3cc079fbfdfe3165f3edbb649fe2
Ashik Ali Shanto <shanto902@gmail.com>
2025-06-14 13:36:47 +0600

    Added Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,26 @@ export default async function RootLayout({
             `,
           }}
         />
+
+        {/* Meta Pixel Script */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1074459663701854');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body className={`${inter.className} antialiased scroll-smooth`}>
         {/* GTM NoScript Fallback */}
@@ -56,6 +76,16 @@ export default async function RootLayout({
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
+        </noscript>
+
+        {/* Facebook Pixel NoScript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1074459663701854&ev=PageView&noscript=1"
+          />
         </noscript>
 
         <NavBar locations={locations} settings={settings as TSettings} />
