9d39acc225b050d0a7a5f872e78ffb36a5bc956b
Ashik Ali Shanto <ashikalishanto@gmail.com>
2025-10-11 01:56:30 +0600

    updated v3

--- components/embedded-form/ConsultancyFrom.tsx
+++ components/embedded-form/ConsultancyFrom.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 const ConsultancyFrom = () => {
   return (
-    <div className="w-full overflow-auto ">
+    <div className="w-full">
       <iframe
         src="https://form.jotform.com/250145363798060"
         width="100%"
-        height="1200"
+        height="1350"
         allow="fullscreen"
+        className=""
       />
     </div>
   );
