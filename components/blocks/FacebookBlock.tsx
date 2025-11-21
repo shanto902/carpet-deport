84558e31dbeb120bd0f6c732fdfad3fe0c7bb24d
Ashik Ali Shanto <ashikalishanto@gmail.com>
2025-11-22 03:56:35 +0600

    facebook added final editable

--- components/blocks/FacebookBlock.tsx
+++ components/blocks/FacebookBlock.tsx
@@ -9,11 +9,14 @@ export default function FacebookBlock({ block }: { block: TFacebookbBlock }) {
     <section className="bg-[#F7F9FA]">
       <PaddingContainer className="mx-auto  py-10">
         <>
-          <div className="flex flex-col md:flex-row justify-between  items-center mb-8">
-            <h2 className="text-3xl font-semibold">{block.item.title}</h2>
+          <div className="flex flex-col md:flex-row justify-between w-full gap-4 items-center mb-4">
+            <h2 className="text-3xl text-center md:text-left font-semibold">
+              {block.item.title}
+            </h2>
             <CustomButton
               button_type="arrow"
               newTab
+              className="w-full md:w-fit"
               href="https://www.facebook.com/carpetdepotatlanta"
             >
               {block.item.button_text}
