440433017c4e841d1d188be4d3025227b886b96c
Ashik Ali Shanto <ashikalishanto@gmail.com>
2026-03-04 03:30:06 +0600

    geolocation fix v6

--- components/pages/location/LocationMap.tsx
+++ components/pages/location/LocationMap.tsx
@@ -65,13 +65,26 @@ const LocationMap = ({ coordinate }: { coordinate: number[] }) => {
 
   if (!isLoaded) return <div>Loading map...</div>;
 
+  const storeMarkerIcon = {
+    url:
+      "data:image/svg+xml;charset=UTF-8," +
+      encodeURIComponent(`
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48">
+        <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="#D32F2F" stroke="#FFFFFF" stroke-width="1.5"/>
+        <circle cx="12" cy="9" r="2.5" fill="#FFFFFF"/>
+      </svg>
+    `),
+    scaledSize: new google.maps.Size(48, 48),
+    anchor: new google.maps.Point(24, 48),
+  };
+
   return (
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={storeLatLng}
       zoom={10}
     >
-      <Marker position={storeLatLng} label="Store" />
+      <Marker position={storeLatLng} icon={storeMarkerIcon} zIndex={100} />
       {userLocation && <Marker position={userLocation} label="You" />}
       {directions && <DirectionsRenderer directions={directions} />}
     </GoogleMap>
